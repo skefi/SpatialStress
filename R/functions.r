3c2ad6087d53278e72ff90f46aa33e316dc7f136
Florian Schneider <florian.schneider@univ-montp2.fr>
2015-06-09 14:08:13 +0200

    add mussel bed model and model selector

--- R/functions.r
+++ R/functions.r
@@ -229,11 +229,118 @@ update_grazing <- function(x_old, parms_temp, delta = 0.2, subs = 10, timestep =
 
 
 
+### mussel bed model (Guichard et al 2003)
+
+
+update_musselbed <- function(x_old, parms_temp, delta = 0.2, subs = 10, timestep = NA) {
+  
+  x_new <- x_old
+  
+  for(s in 1:subs) {
+    
+    parms_temp$rho_plus <- sum(x_old$cells == "+")/(x_old$dim[1]*x_old$dim[2]) # get initial vegetation cover
+    parms_temp$localdisturbance <- count(x_old, "-") > 0  # any disturbance in neighborhood?
+    parms_temp$localcover <- count(x_old, "+")/4 # any occupied in neighborhood? 
+    
+    # 2 - drawing random numbers
+    rnum <- runif(x_old$dim[1]*x_old$dim[2]) # one random number between 0 and 1 for each cell
+    
+    # 3 - setting transition probabilities
+    
+    if(parms_temp$rho_plus > 0) {
+      recolonisation <- with(parms_temp, (r*localcover)*1/subs)
+      
+      disturbance <- with(parms_temp, (delta+d*localdisturbance)*1/subs)
+      disturbance[disturbance > 1] <- 1 
+    } else {
+      recolonisation <- 0
+      disturbance <- 1
+    }
+    
+    regeneration <- 1*1/subs
+    
+    # check for sum of probabilities to be inferior 1 and superior 0
+    if(any(c(recolonisation, disturbance, regeneration) > 1 )) warning(paste("a set probability is exceeding 1 in  time step", timestep, "! decrease number of substeps!!!")) 
+    if(any(recolonisation < 0)) warning(paste("recolonisation falls below 0 in time step",timestep, "! balance parameters!!!")) 
+    if(any( disturbance < 0)) warning(paste("disturbance falls below 0 in time step",timestep, "! balance parameters!!!")) 
+    if(any(regeneration < 0)) warning(paste("regeneration falls below 0 in time step",timestep, "! balance parameters!!!")) 
+    
+    # 4 - apply transition probabilities  
+    
+    x_new$cells[which(x_old$cells == "0" & rnum <= recolonisation)] <- "+"
+    x_new$cells[which(x_old$cells == "+"  & rnum <= disturbance)] <- "-"
+    x_new$cells[which(x_old$cells == "-"   & rnum <= regeneration)] <- "0"
+    
+    # 5- store x_new as next x_old
+    
+    x_old <- x_new
+    
+  }
+  
+  ## end of single update call
+  return(x_new)
+}
+
+### predator prey model (Pascual et al, 2002)
+
+update_predprey <- function(x_old, parms_temp, subs = 10, timestep = NA) {
+  
+  x_new <- x_old
+  
+  for(s in 1:subs) {
+    
+    parms_temp$q_prey <- count(x_old, "prey")/4  # count local density of occupied fields for each cell:
+    
+    parms_temp$q_pred <- count(x_old, "pred")/4  # count local density of occupied fields for each cell:
+    
+    # 2 - drawing random numbers
+    rnum <- runif(x_old$dim[1]*x_old$dim[2]) # one random number between 0 and 1 for each cell
+    
+    # 3 - setting transition probabilities
+    
+    if(parms_temp$rho_plus > 0) {
+      recolonisation <- with(parms_temp, (del*rho_plus+(1-del)*Q_plus)*(b-c_*rho_plus)*1/subs)
+      
+      death <- with(parms_temp, (m0+g*(1-protect))*1/subs)
+      death[death > 1] <- 1 
+    } else {
+      recolonisation <- 0
+      death <- 1
+    }
+    
+    regeneration <- with(parms_temp, (r + f*Q_plus)*1/subs)
+    
+    degradation <- with(parms_temp, (d*1/subs))
+    
+    # check for sum of probabilities to be inferior 1 and superior 0
+    if(any(c(recolonisation+degradation, death, regeneration) > 1 )) warning(paste("a set probability is exceeding 1 in  time step", timestep, "! decrease number of substeps!!!")) 
+    if(any(recolonisation < 0)) warning(paste("recolonisation falls below 0 in time step",timestep, "! balance parameters!!!")) 
+    if(any(degradation < 0)) warning(paste("degradation falls below 0 in time step",timestep, "! balance parameters!!!"))
+    if(any( death < 0)) warning(paste("death falls below 0 in time step",timestep, "! balance parameters!!!")) 
+    if(any(regeneration < 0)) warning(paste("regeneration falls below 0 in time step",timestep, "! balance parameters!!!")) 
+    
+    # 4 - apply transition probabilities  
+    
+    x_new$cells[which(x_old$cells == "0" & rnum <= recolonisation)] <- "+"
+    x_new$cells[which(x_old$cells == "+"  & rnum <= death)] <- "0"
+    x_new$cells[which(x_old$cells == "0"  & rnum > recolonisation & rnum <= recolonisation+degradation)] <- "-"  
+    x_new$cells[which(x_old$cells == "-"   & rnum <= regeneration)] <- "0"
+    
+    # 5- store x_new as next x_old
+    
+    x_old <- x_new
+    
+  }
+  
+  ## end of single update call
+  return(x_new)
+}
+
 ################################
 ## run simulation of CA model ##
 ################################
 
-ca <- function(x, parms, delta = 0.1, t_max = 1000, t_min = 500, t_eval = 200, isstable = 0.000001, saveeach = 50) {
+ca <- function(x, parms, delta = 0.1, t_max = 1000, t_min = 500, t_eval = 200, isstable = 0.000001, saveeach = 50, model = update_musselbed )  {
   
   mapping(x$dim[1], x$dim[2])
   xstats <- summary(x)
@@ -272,7 +379,7 @@ ca <- function(x, parms, delta = 0.1, t_max = 1000, t_min = 500, t_eval = 200, i
 
     i <- i +1  # increase iterator
     
-    update_grazing(x_old, parms, 10) -> x_new
+    model(x_old, parms, 10) -> x_new
     
     xstats <- summary(x_new)
     result$cover[i,] <- xstats$cover
